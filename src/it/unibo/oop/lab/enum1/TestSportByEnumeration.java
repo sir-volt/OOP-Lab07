7ffe7d176a26c12fe565ccd2dd79d48892e3bc7f
Andrea Bianchi - andrea.bianchi24@studio.unibo.it <andrea.bianchi24@studio.unibo.it>
2021-11-08 11:52:59 +0100

    added some users with a specific set of sport for each of them in order to test the correct use of enum Sport

--- src/it/unibo/oop/lab/enum1/TestSportByEnumeration.java
+++ src/it/unibo/oop/lab/enum1/TestSportByEnumeration.java
@@ -1,5 +1,7 @@
 package it.unibo.oop.lab.enum1;
 
+import it.unibo.oop.lab.socialnetwork.User;
+
 /**
  * This is going to act as a test for
  * {@link it.unibo.oop.lab.enum1.SportSocialNetworkUserImpl}
@@ -25,5 +27,33 @@ public final class TestSportByEnumeration {
          * By now, you know how to do it
          */
         // TODO
+    	
+    	final SportSocialNetworkUserImpl<User> giovanni = new SportSocialNetworkUserImpl<>("Giovanni","Muchacha","artattack",49);
+    	final SportSocialNetworkUserImpl<User> claudio = new SportSocialNetworkUserImpl<>("Claudio","Bisio","benvenutoPresidente",53);
+    	final SportSocialNetworkUserImpl<User> luigi = new SportSocialNetworkUserImpl<>("Luigi","Mario","greenMario",89);
+    	/*
+    	 * TEST ON GIOVANNI
+    	 */
+    	giovanni.addSport(Sport.BASKET);
+    	giovanni.addSport(Sport.BIKE);
+    	giovanni.addSport(Sport.F1);
+    	System.out.println("giovanni è un fan del basket:" + giovanni.hasSport(Sport.BASKET));
+    	System.out.println("giovanni va spesso in bicicletta:" + giovanni.hasSport(Sport.BIKE));
+    	System.out.println("giovanni NON segue la motoGP:" + !giovanni.hasSport(Sport.MOTOGP));
+    	/*
+    	 * TEST ON CLAUDIO
+    	 */
+    	claudio.addSport(Sport.MOTOGP);
+    	claudio.addSport(Sport.SOCCER);
+    	System.out.println("claudio NON è un fan del basket:" + !claudio.hasSport(Sport.BASKET));
+    	System.out.println("claudio gioca a calcio:" + claudio.hasSport(Sport.SOCCER));
+    	System.out.println("il presidente segue la motoGP:" + claudio.hasSport(Sport.MOTOGP));
+    	/*
+    	 * TEST ON LUIGI
+    	 */
+    	luigi.addSport(Sport.TENNIS);
+    	luigi.addSport(Sport.VOLLEY);
+    	System.out.println("luigi segue pallavolo:" + luigi.hasSport(Sport.VOLLEY));
+    	System.out.println("il fratello minore è un asso a tennis:" + luigi.hasSport(Sport.TENNIS));	
     }
 }
