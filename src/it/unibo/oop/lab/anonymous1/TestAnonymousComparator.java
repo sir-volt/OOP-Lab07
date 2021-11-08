cb2e00be4b233394652460ee657e4f69dd0417c9
Andrea Bianchi - andrea.bianchi24@studio.unibo.it <andrea.bianchi24@studio.unibo.it>
2021-11-08 10:53:44 +0100

    added the rest of the lab/ files

--- src/it/unibo/oop/lab/anonymous1/TestAnonymousComparator.java
+++ src/it/unibo/oop/lab/anonymous1/TestAnonymousComparator.java
@@ -1,6 +1,8 @@
 package it.unibo.oop.lab.anonymous1;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import it.unibo.oop.lab.socialnetwork.SocialNetworkUser;
@@ -74,7 +76,13 @@ public final class TestAnonymousComparator {
          * 
          * REFER TO LESSON 13-Advanced-Mechanisms.pdf, slide 41
          */
-        // TODO
+        denzelUsers.sort(new Comparator<User>() {
+			@Override
+			public int compare(User o1, User o2) {
+				return o1.getAge() - o2.getAge();
+			}
+        	
+        });
         /*
          * expected Result
          */
@@ -105,7 +113,14 @@ public final class TestAnonymousComparator {
          * NOTE: in order to sort a list think about a method of the utility
          * class Collections
          */
-        // TODO
+        Collections.sort(rossiUsers, new Comparator<User>() {
+
+			@Override
+			public int compare(User o1, User o2) {
+				return o1.getAge() - o2.getAge();
+			}
+        	
+        }.reversed());
         /*
          * expected Result
          */
